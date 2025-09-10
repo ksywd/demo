a552f5136dd2ced2772dbacc9bbd04805ad06e2a
ksywd <k01040366905@gmail.com>
2025-09-10 10:42:14 +0900

    DAY1_PROJECT

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,4 @@ public class DemoApplication { // 클래스 이름
 	public static void main(String[] args) {  // 메인 메서드(프로그램 시작점)
 		SpringApplication.run(DemoApplication.class, args); // run 메서드로 실행
 	}
-
 }
