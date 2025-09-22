ba37bdb373239da96c60e9a115268e558f9a1faf
ksywd <k01040366905@gmail.com>
2025-09-22 12:43:55 +0900

    3주차 연습문제

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -12,8 +12,21 @@ public class DemoController {
         model.addAttribute("data", "방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
     }
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("name", "홍길동");
+        model.addAttribute("greeting", "방갑습니다.");
+        model.addAttribute("today", "오늘");
+        model.addAttribute("weather", "날씨는");
+        model.addAttribute("comment", "매우 좋습니다.");
+        return "hello2"; // templates/hello2.html 로 연결
+    }
     @GetMapping("/about_detailed")
         public String about() {
         return "about_detailed";
+    }
+    @GetMapping("/index")
+    public String index() {
+        return "index";
     }
 }
