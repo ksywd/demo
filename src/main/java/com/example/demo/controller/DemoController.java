13553b47083b463f5a007be5746b8dda9778bbc7
ksywd <k01040366905@gmail.com>
2025-10-01 11:05:07 +0900

    5주차

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
+
+    @Autowired
+    private TestService testService; // DemoController 클래스 아래 객체 생성
 
     @GetMapping("/hello") // 전송 방식 GET
     public String hello(Model model) {
@@ -39,5 +46,16 @@ public class DemoController {
         model.addAttribute("para1", "001");
         model.addAttribute("para2", 002);
         return "thymeleaf_test1";
+    }
+        // 하단에 맵핑 이어서 추가
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+
+        System.out.println("데이터 출력 디버그 : " + test);
+
+        // "뷰이름"은 test.html 같은 템플릿 파일 이름으로 교체해야 함
+        return "testdb";
     }
 }
