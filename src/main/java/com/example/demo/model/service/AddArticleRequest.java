1f3decb319e5f5c4e5298a1e734ad7491d044cba
ksywd <k01040366905@gmail.com>
2025-11-19 11:25:31 +0900

    10주차(미완)

--- src/main/java/com/example/demo/model/service/AddArticleRequest.java
+++ src/main/java/com/example/demo/model/service/AddArticleRequest.java
@@ -4,10 +4,9 @@ import lombok.*; // 어노테이션 자동 생성
 import com.example.demo.model.domain.Board;
 import com.example.demo.model.domain.Article;
 
-
-@NoArgsConstructor  // 기본 생성자 추가
-@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
-@Data               // getter, setter, toString, equals 등 자동 생성
+@NoArgsConstructor      // 기본 생성자 추가
+@AllArgsConstructor     // 모든 필드 값을 파라미터로 받는 생성자 추가
+@Data                   // getter, setter, toString, equals 등 자동 생성
 public class AddArticleRequest {
 
     private String title;
@@ -19,12 +18,12 @@ public class AddArticleRequest {
 
     public Board toEntity() { // Article 객체 생성
         return Board.builder()
-                    .title(title)
-                    .content(content)
-                    .user(user)
-                    .newdate(newdate)
-                    .count(count)
-                    .likec(likec)
-                    .build();
+                .title(title)
+                .content(content)
+                .user(user)
+                .newdate(newdate)
+                .count(count)
+                .likec(likec)
+                .build();
     }
 }
---
+++ src/main/java/com/example/demo/model/service/AddMemberRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.model.service;
+
+import com.example.demo.model.domain.Member;
+
+public class AddMemberRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String age;
+    private String mobile;
+    private String address;
+
+    public Member toEntity() { // Member 생성자를 통해 객체 생성
+        return Member.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .age(age)
+                .mobile(mobile)
+                .address(address)
+                .build();
+    }
+}
